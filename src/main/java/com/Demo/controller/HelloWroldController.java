ef68918e7f3a3aa6fcd20b04f2827520369be382
syedmairaj <syedmairaj@yahoo.com>
2025-09-08 19:39:30 +0400

    demo-app-v1

---
+++ src/main/java/com/Demo/controller/HelloWroldController.java
@@ -0,0 +1,14 @@
+package com.Demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWroldController {
+
+	@GetMapping("/message")
+public String getMessage() {
+	return " hellow world";
+}
+
+}
